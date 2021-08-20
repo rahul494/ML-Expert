5514ea8ff70a417721d7959606d9a4a9302a62db
rahul494 <rahul494@my.yorku.ca>
2021-08-20 15:24:31 -0400

    Initialized get_statistics.py

---
+++ Math Concepts/Get Statistics/get_statistics.py
@@ -0,0 +1,10 @@
+def get_statistics(input_list):
+    # Write your code here.
+    return {
+        "mean": 0,
+        "median": 0,
+        "mode": 0,
+        "sample_variance": 0,
+        "sample_standard_deviation": 0,
+        "mean_confidence_interval": [0, 0],
+    }
